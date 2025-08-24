cc61599c3194421b3f777ce3295b5bd4d2882444
MeenakshiSagar <meenakshisagar418@gmail.com>
2025-08-24 22:45:11 +0530

    reverse number program

--- learn_to_Automate/src/learn_to_Automate/string_programs/ReverseString.java
+++ learn_to_Automate/src/learn_to_Automate/string_programs/ReverseString.java
@@ -4,6 +4,20 @@ public class ReverseString {
 
     public static void main(String[] args) {
 
+        int number = 123456;
+        System.out.println(reverseNumberFunction(number));
+    }
 
+    public static int reverseNumberFunction(int number){
+
+        int reverseNumber = 0;
+        while(number > 0){
+
+            int lastDigit = number % 10;
+            reverseNumber = reverseNumber * 10 + lastDigit;
+            number = number / 10;
+        }
+
+        return reverseNumber;
     }
 }
